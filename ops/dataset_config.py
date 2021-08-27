cdbcac2e81cc3710b9c2727acf1e618da92fd0cc
benbwf <benbwf@users.noreply.github.com>
2021-08-27 14:41:49 +0800

    changed root_dataset variable

--- ops/dataset_config.py
+++ ops/dataset_config.py
@@ -5,104 +5,10 @@
 
 import os
 
-# ROOT_DATASET = 'C:/Users/Admin/dev/data'  # '/data/jilin/'
-
-
-# def return_ucf101(modality):
-#     filename_categories = 'UCF101/labels/classInd.txt'
-#     if modality == 'RGB':
-#         root_data = ROOT_DATASET + 'UCF101/jpg'
-#         filename_imglist_train = 'UCF101/file_list/ucf101_rgb_train_split_1.txt'
-#         filename_imglist_val = 'UCF101/file_list/ucf101_rgb_val_split_1.txt'
-#         prefix = 'img_{:05d}.jpg'
-#     elif modality == 'Flow':
-#         root_data = ROOT_DATASET + 'UCF101/jpg'
-#         filename_imglist_train = 'UCF101/file_list/ucf101_flow_train_split_1.txt'
-#         filename_imglist_val = 'UCF101/file_list/ucf101_flow_val_split_1.txt'
-#         prefix = 'flow_{}_{:05d}.jpg'
-#     else:
-#         raise NotImplementedError('no such modality:' + modality)
-#     return filename_categories, filename_imglist_train, filename_imglist_val, root_data, prefix
-
-
-# def return_hmdb51(modality):
-#     filename_categories = 51
-#     if modality == 'RGB':
-#         root_data = ROOT_DATASET + 'HMDB51/images'
-#         filename_imglist_train = 'HMDB51/splits/hmdb51_rgb_train_split_1.txt'
-#         filename_imglist_val = 'HMDB51/splits/hmdb51_rgb_val_split_1.txt'
-#         prefix = 'img_{:05d}.jpg'
-#     elif modality == 'Flow':
-#         root_data = ROOT_DATASET + 'HMDB51/images'
-#         filename_imglist_train = 'HMDB51/splits/hmdb51_flow_train_split_1.txt'
-#         filename_imglist_val = 'HMDB51/splits/hmdb51_flow_val_split_1.txt'
-#         prefix = 'flow_{}_{:05d}.jpg'
-#     else:
-#         raise NotImplementedError('no such modality:' + modality)
-#     return filename_categories, filename_imglist_train, filename_imglist_val, root_data, prefix
-
-
-# def return_something(modality):
-#     filename_categories = 'something/v1/category.txt'
-#     if modality == 'RGB':
-#         root_data = ROOT_DATASET + 'something/v1/20bn-something-something-v1'
-#         filename_imglist_train = 'something/v1/train_videofolder.txt'
-#         filename_imglist_val = 'something/v1/val_videofolder.txt'
-#         prefix = '{:05d}.jpg'
-#     elif modality == 'Flow':
-#         root_data = ROOT_DATASET + 'something/v1/20bn-something-something-v1-flow'
-#         filename_imglist_train = 'something/v1/train_videofolder_flow.txt'
-#         filename_imglist_val = 'something/v1/val_videofolder_flow.txt'
-#         prefix = '{:06d}-{}_{:05d}.jpg'
-#     else:
-#         print('no such modality:'+modality)
-#         raise NotImplementedError
-#     return filename_categories, filename_imglist_train, filename_imglist_val, root_data, prefix
-
-
-# def return_somethingv2(modality):
-#     filename_categories = 'something/v2/category.txt'
-#     if modality == 'RGB':
-#         root_data = ROOT_DATASET + 'something/v2/20bn-something-something-v2-frames'
-#         filename_imglist_train = 'something/v2/train_videofolder.txt'
-#         filename_imglist_val = 'something/v2/val_videofolder.txt'
-#         prefix = '{:06d}.jpg'
-#     elif modality == 'Flow':
-#         root_data = ROOT_DATASET + 'something/v2/20bn-something-something-v2-flow'
-#         filename_imglist_train = 'something/v2/train_videofolder_flow.txt'
-#         filename_imglist_val = 'something/v2/val_videofolder_flow.txt'
-#         prefix = '{:06d}.jpg'
-#     else:
-#         raise NotImplementedError('no such modality:'+modality)
-#     return filename_categories, filename_imglist_train, filename_imglist_val, root_data, prefix
-
-
-# def return_jester(modality):
-#     filename_categories = 'jester/category.txt'
-#     if modality == 'RGB':
-#         prefix = '{:05d}.jpg'
-#         root_data = ROOT_DATASET + 'jester/20bn-jester-v1'
-#         filename_imglist_train = 'jester/train_videofolder.txt'
-#         filename_imglist_val = 'jester/val_videofolder.txt'
-#     else:
-#         raise NotImplementedError('no such modality:'+modality)
-#     return filename_categories, filename_imglist_train, filename_imglist_val, root_data, prefix
-
-
-def return_kinetics(modality, version):
-    filename_categories = 400
-    if modality == 'RGB':
-        root_data = ROOT_DATASET + 'kinetics400/frames' # /ssd/video/kinetics/images/ #class/video_id/image.jpeg
-        filename_imglist_train = 'kinetics400/train_videofolder.txt' # contents: path numframes label_num
-        filename_imglist_val = 'kinetics400/val_videofolder.txt'
-        prefix = 'img_{:05d}.jpg'
-    else:
-        raise NotImplementedError('no such modality:' + modality)
-    return filename_categories, filename_imglist_train, filename_imglist_val, root_data, prefix
 
 def return_ite(modality, version):
     if version is not None:
-        with open (os.path.join(ROOT_DATASET, 'ite_dataset', version, 'actions_label_map.txt'), 'r') as file:
+        with open (os.path.join(ROOT_DATASET, version, 'actions_label_map.txt'), 'r') as file:
             catigories = file.readlines()
             filename_categories = len(catigories)
         if modality == 'RGB':
@@ -157,10 +63,7 @@ def return_ek100(modality, version):
 def return_dataset(root, dataset, modality, version = None):
     global ROOT_DATASET
     ROOT_DATASET = root
-    dict_single = {
-        # 'jester': return_jester, 'something': return_something, 'somethingv2': return_somethingv2,
-        #            'ucf101': return_ucf101, 'hmdb51': return_hmdb51,
-                   'kinetics': return_kinetics, 'ite': return_ite, 'ek100': return_ek100 }
+    dict_single = {'ite': return_ite, 'ek100': return_ek100 }
     if dataset in dict_single:
         file_categories, file_imglist_train, file_imglist_val, root_data, prefix = dict_single[dataset](modality, version)
     else:
